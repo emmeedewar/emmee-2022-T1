06ecdbaeb53f468ff205bf06792482ee54e1f718
emmeedewar <100326329+emmeedewar@users.noreply.github.com>
2022-04-14 12:42:49 +1000

    added js

---
+++ resources/js/scripts.js
@@ -0,0 +1,13 @@
+/* nav */
+function openNav() {
+
+    let toggler = document.querySelector('.nav-prompt')
+
+    let navigation = document.querySelector('.main-nav')
+
+    toggler.onclick = function() {
+        navigation.classList.toggle('open')
+    }
+}
+
+openNav()
